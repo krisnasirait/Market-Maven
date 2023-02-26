e8bc75601a95f6c7e6e3a8b9661dd9a275ee15dd
krisna <krisnasirait48@gmail.com>
2023-02-26 21:03:52 +0700

    added action btn about in action bar

--- app/src/main/java/com/krisna/marketmaven/MainActivity.kt
+++ app/src/main/java/com/krisna/marketmaven/MainActivity.kt
@@ -2,6 +2,8 @@ package com.krisna.marketmaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.krisna.marketmaven.databinding.ActivityMainBinding
 
@@ -15,5 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_about -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
