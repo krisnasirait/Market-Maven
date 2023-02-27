ddfb210b7b779c6b24b8a49e05a01ce4681a14e4
krisna <krisnasirait48@gmail.com>
2023-02-27 14:19:20 +0700

    working rv, to show item from DataArticle.kt

--- app/src/main/java/com/krisna/marketmaven/MainActivity.kt
+++ app/src/main/java/com/krisna/marketmaven/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.krisna.marketmaven
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.krisna.marketmaven.adapter.ArticleAdapter
+import com.krisna.marketmaven.data.DataArticle
 import com.krisna.marketmaven.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rv: RecyclerView
+    private lateinit var adapterArticle: ArticleAdapter
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +24,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        rv = binding.rvArticle
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.setHasFixedSize(true)
+
+        adapterArticle = ArticleAdapter()
+        adapterArticle.setDataArticle(DataArticle.itemArticle)
+        rv.adapter = adapterArticle
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/krisna/marketmaven/data/DataArticle.kt
+++ app/src/main/java/com/krisna/marketmaven/data/DataArticle.kt
@@ -3,6 +3,47 @@ package com.krisna.marketmaven.data
 import com.krisna.marketmaven.R
 import com.krisna.marketmaven.data.model.Article
 
-val article = listOf(
-    Article("testAuthor", "Test Content", "Test content true", "28th Oct 2022", "Test Title", R.drawable.news)
-)
+object DataArticle {
+    val itemArticle : ArrayList<Article> = arrayListOf (
+        Article(
+            "John Smith",
+            "Stocks surge as earnings reports beat expectations",
+            "Stocks jumped to record highs as a string of strong earnings reports from major companies exceeded Wall Street's expectations. The Dow Jones Industrial Average rose 500 points, or 1.5%, while the S&P 500 and Nasdaq Composite also gained 1.5%.",
+            "2022-02-25T18:00:00Z",
+            "Earnings reports beat expectations",
+            R.drawable.article1
+        ),
+        Article(
+            "Jane Doe",
+            "Oil prices continue to climb amid supply concerns",
+            "Oil prices rose to a fresh 7-year high as supply concerns persisted due to ongoing tensions in the Middle East and Russia's recent decision to cut oil exports. Brent crude, the international benchmark, climbed 2% to $90.50 per barrel.",
+            "2022-02-25T17:30:00Z",
+            "Oil prices climb amid supply concerns",
+            R.drawable.article1
+        ),
+        Article(
+            "David Johnson",
+            "Retail sales fall as consumers cut back on spending",
+            "US retail sales fell 0.5% in January as consumers cut back on spending, according to the Commerce Department. The drop was larger than expected and was the biggest decline since the start of the pandemic.",
+            "2022-02-25T17:00:00Z",
+            "Retail sales fall in January",
+            R.drawable.article1
+        ),
+        Article(
+            "Emily Davis",
+            "GameStop stock surges on earnings beat",
+            "GameStop shares surged 25% after the company reported better-than-expected earnings for the fourth quarter. The video game retailer also announced plans to expand into online gaming and non-fungible tokens (NFTs).",
+            "2022-02-24T16:00:00Z",
+            "GameStop stock surges on earnings beat",
+            R.drawable.article1
+        ),
+        Article(
+            "James Lee",
+            "Bitcoin hits new all-time high above $60,000",
+            "Bitcoin reached a new all-time high of $60,450 as the world's largest cryptocurrency continued its upward trajectory. The surge was driven by increased adoption by institutional investors and positive sentiment around the technology.",
+            "2022-02-24T15:30:00Z",
+            "Bitcoin hits new all-time high",
+            R.drawable.article1
+        ),
+            )
+}
