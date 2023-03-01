47f9d659fd991da6bc78e2f6fcd146cdb7c6e644
krisna <krisnasirait48@gmail.com>
2023-03-01 17:35:50 +0700

    - added poster for item news - changes for item_article.xml layout, to show the image better

--- app/src/main/java/com/krisna/marketmaven/data/DataArticle.kt
+++ app/src/main/java/com/krisna/marketmaven/data/DataArticle.kt
@@ -19,7 +19,7 @@ object DataArticle {
             "Oil prices rose to a fresh 7-year high as supply concerns persisted due to ongoing tensions in the Middle East and Russia's recent decision to cut oil exports. Brent crude, the international benchmark, climbed 2% to $90.50 per barrel.",
             "25 February 2023",
             "Oil prices climb amid supply concerns",
-            R.drawable.article1
+            R.drawable.article2
         ),
         Article(
             "David Johnson",
@@ -27,7 +27,7 @@ object DataArticle {
             "US retail sales fell 0.5% in January as consumers cut back on spending, according to the Commerce Department. The drop was larger than expected and was the biggest decline since the start of the pandemic.",
             "27 February 2023",
             "Retail sales fall in January",
-            R.drawable.article1
+            R.drawable.article3
         ),
         Article(
             "Emily Davis",
@@ -35,7 +35,7 @@ object DataArticle {
             "GameStop shares surged 25% after the company reported better-than-expected earnings for the fourth quarter. The video game retailer also announced plans to expand into online gaming and non-fungible tokens (NFTs).",
             "15 February 2023",
             "GameStop stock surges on earnings beat",
-            R.drawable.article1
+            R.drawable.article4
         ),
         Article(
             "James Lee",
@@ -43,7 +43,7 @@ object DataArticle {
             "Bitcoin reached a new all-time high of $60,450 as the world's largest cryptocurrency continued its upward trajectory. The surge was driven by increased adoption by institutional investors and positive sentiment around the technology.",
             "17 February 2023",
             "Bitcoin hits new all-time high",
-            R.drawable.article1
+            R.drawable.article5
         ),
         Article(
             "Sarah Kim",
@@ -51,7 +51,7 @@ object DataArticle {
             "US inflation rose to its highest level in 39 years in January, according to data from the Labor Department. The Consumer Price Index (CPI) increased by 7% compared to the previous year, driven by rising energy and food prices.",
             "19 February 2023",
             "Inflation rises to highest level in 39 years",
-            R.drawable.article1
+            R.drawable.article6
         ),
         Article(
             "John Lee",
@@ -59,7 +59,7 @@ object DataArticle {
             "Stocks around the world fell sharply as investors grew concerned about the impact of rising interest rates on corporate earnings. The Dow Jones Industrial Average dropped 2.5%, while the S&P 500 and Nasdaq Composite each lost 2.7%.",
             "12 February 2023",
             "Global stocks tumble on interest rate fears",
-            R.drawable.article1
+            R.drawable.article7
         ),
         Article(
             "Ava Patel",
@@ -67,7 +67,7 @@ object DataArticle {
             "Shares of technology companies surged as several big players reported better-than-expected earnings. Apple, Amazon, and Google parent Alphabet all saw their stock prices jump by more than 4%, helping to push the Nasdaq Composite up by 2.5%.",
             "2 February 2023",
             "Tech stocks rally on earnings reports",
-            R.drawable.article1
+            R.drawable.article8
         ),
         Article(
             "David Kim",
@@ -75,7 +75,7 @@ object DataArticle {
             "The number of Americans filing for unemployment benefits fell to a new pandemic low of 200,000 last week, according to the Labor Department. The figure was lower than expected and suggests that the labor market is continuing to recover from the pandemic.",
             "09 February 2023",
             "US jobless claims fall to pandemic low",
-            R.drawable.article1
+            R.drawable.article9
         ),
         Article(
             "Sophia Lee",
@@ -83,7 +83,7 @@ object DataArticle {
             "China's economic growth slowed to 4% in the fourth quarter, down from 4.9% in the previous quarter, as the government's efforts to cool the property sector began to take effect. The figure was in line with expectations and reflects the ongoing challenges facing the world's second-largest economy.",
             "07 February 2023",
             "China's economy slows as property sector cools",
-            R.drawable.article1
+            R.drawable.article10
         )
             )
 }
